43a07059a420ac5cc91e7d30970f4cfd95c77880
Adhish Shrivastava <adhish70@gmail.com>
2022-01-08 12:29:31 -0500

    Initial Commit

---
+++ Problem-157 Critical Connections in a Network.py
@@ -0,0 +1,11 @@
+# 1192. Critical Connections in a Network
+# https://leetcode.com/problems/critical-connections-in-a-network/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        pass
